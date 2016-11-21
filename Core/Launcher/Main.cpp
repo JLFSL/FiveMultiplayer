352f710c875d269bb58a9734f2caf87804d7f479
FiveMP <developer@five-multiplayer.net>
2016-11-21 18:51:38 +0100

    Added Core.Launcher and Vendor.CEF

---
+++ Core/Launcher/Main.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	CefEnableHighDPISupport();
+
+	return 0;
+}
---
+++ Core/Launcher/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef _WIN32
+// Win
+#include <Windows.h>
+
+// CEF
+#include "include/cef_app.h"
+#include "include/cef_sandbox_win.h"
+
+#endif
+
