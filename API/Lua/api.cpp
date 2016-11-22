7447e91eab663ecf26446969960a1ca765ba0aff
FiveMP <developer@five-multiplayer.net>
2016-11-22 18:52:42 +0100

    Removed define BUILDING_DLL in API.Lua

--- API/Lua/api.cpp
+++ API/Lua/api.cpp
@@ -3,22 +3,11 @@
 #include <iostream>
 using namespace std;
 
-#ifndef BUILDING_DLL
-#define BUILDING_DLL
-#endif
 #if defined _WIN32 || defined __CYGWIN__
-#ifdef BUILDING_DLL
 #ifdef __GNUC__
 #define DLL_PUBLIC __attribute__ ((dllexport))
 #else
 #define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
-#endif
-#else
-#ifdef __GNUC__
-#define DLL_PUBLIC __attribute__ ((dllimport))
-#else
-#define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
-#endif
 #endif
 #define DLL_LOCAL
 #else
