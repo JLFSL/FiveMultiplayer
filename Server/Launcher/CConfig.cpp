7e1bea0bf30887b756215f19c02259baa7618a99
FiveMP <developer@five-multiplayer.net>
2016-12-21 22:35:28 +0100

    ..

--- Server/Launcher/CConfig.cpp
+++ Server/Launcher/CConfig.cpp
@@ -4,10 +4,12 @@ bool CConfig::Read()
 {
 	INIReader Config("Server.Config.ini");
 
+	bool fail = false;
+
 	if (Config.ParseError() < 0) {
 		cout << "[CConfig] Can't read configuration file" << endl;
+		fail = true;
 #ifdef NDEBUG
-		return false;
 	}
 	else 
 	{
@@ -28,6 +30,24 @@ bool CConfig::Read()
 
 		cout << "[CConfig] Read config" << endl;
 		return true;
+	}
+	
+	if (!fail) {
+		cout << "[CConfig] Loaded config" << endl;
+
+		Connection.Ip = Config.Get("Connection", "ip", "188.166.76.252");
+		Connection.Port = Config.GetInteger("Connection", "port", 2322);
+		Connection.Pass = Config.Get("Connection", "pass", "default");
+		Connection.Announce = Config.GetBoolean("Connection", "announce", false);
+
+		Limits.MaxPlayers = Config.GetInteger("Limits", "players", 50);
+
+		Information.Name = Config.Get("Information", "Name", "FiveMP Server (" INFO_VERSION ")");
+
+		Plugins.Language = Config.Get("Plugins", "lang", "API.Lua");
+		Plugins.Additional = Config.Get("Plugins", "etc", "");
+
+		cout << "[CConfig] Read config" << endl;
 	}
 	return false;
 }
