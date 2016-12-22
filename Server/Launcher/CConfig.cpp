391b688559e8d976287e89d246ceb1172fd45ed6
FiveMP <developer@five-multiplayer.net>
2016-12-22 19:47:47 +0100

    Server.Launcher: Improves stability of CConfig

--- Server/Launcher/CConfig.cpp
+++ Server/Launcher/CConfig.cpp
@@ -4,11 +4,9 @@ bool CConfig::Read()
 {
 	INIReader Config("Server.Config.ini");
 
-	bool fail = false;
-
 	if (Config.ParseError() < 0) {
 		cout << "[CConfig] Can't read configuration file" << endl;
-		fail = true;
+		return false;
 #ifdef NDEBUG
 	}
 	else 
@@ -22,25 +20,6 @@ bool CConfig::Read()
 		Connection.Announce = Config.GetBoolean("Connection", "announce", false);
 
 		Limits.MaxPlayers	= Config.GetInteger("Limits", "players", 50);
-
-		Information.Name = Config.Get("Information", "Name", "FiveMP Server (" INFO_VERSION ")");
-
-		Plugins.Language = Config.Get("Plugins", "lang", "API.Lua");
-		Plugins.Additional = Config.Get("Plugins", "etc", "");
-
-		cout << "[CConfig] Read config" << endl;
-		return true;
-	}
-	
-	if (!fail) {
-		cout << "[CConfig] Loaded config" << endl;
-
-		Connection.Ip = Config.Get("Connection", "ip", "188.166.76.252");
-		Connection.Port = Config.GetInteger("Connection", "port", 2322);
-		Connection.Pass = Config.Get("Connection", "pass", "default");
-		Connection.Announce = Config.GetBoolean("Connection", "announce", false);
-
-		Limits.MaxPlayers = Config.GetInteger("Limits", "players", 50);
 
 		Information.Name = Config.Get("Information", "Name", "FiveMP Server (" INFO_VERSION ")");
 
