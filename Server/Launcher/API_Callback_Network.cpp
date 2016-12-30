042c81737c3271ee6efac7fd0ad2856186b4d7cc
FiveMP <developer@five-multiplayer.net>
2016-12-30 22:22:41 +0100

    Server.Launcher: Fixed an issue where the return value was not sent

--- Server/Launcher/API_Callback_Network.cpp
+++ Server/Launcher/API_Callback_Network.cpp
@@ -10,16 +10,13 @@ namespace API
 		{
 			if (Instance)
 			{
-				typedef void(*API_OnPlayerConnecting_t)(const char*);
+				typedef bool(*API_OnPlayerConnecting_t)(const char*);
 #ifdef WIN32
 				API_OnPlayerConnecting_t API_OnPlayerConnecting = (API_OnPlayerConnecting_t)::GetProcAddress((HMODULE)Instance, "API_OnPlayerConnecting");
 #else
 				API_OnPlayerConnecting_t API_OnPlayerConnecting = (API_OnPlayerConnecting_t)dlsym(&Instance, "API_OnPlayerConnecting");
 #endif
-				API_OnPlayerConnecting(guid);
-				if (!API_OnPlayerConnecting)
-					return false;
-				return true;
+				return API_OnPlayerConnecting(guid);
 			}
 			return false;
 		}
@@ -28,16 +25,13 @@ namespace API
 		{
 			if (Instance)
 			{
-				typedef void(*API_OnPlayerConnected_t)();
+				typedef bool(*API_OnPlayerConnected_t)();
 #ifdef WIN32
 				API_OnPlayerConnected_t API_OnPlayerConnected = (API_OnPlayerConnected_t)::GetProcAddress((HMODULE)Instance, "API_OnPlayerConnected");
 #else
 				API_OnPlayerConnected_t API_OnPlayerConnected = (API_OnPlayerConnected_t)dlsym(&Instance, "API_OnPlayerConnected");
 #endif
-				API_OnPlayerConnected();
-				if (!API_OnPlayerConnected)
-					return false;
-				return true;
+				return API_OnPlayerConnected();
 			}
 			return false;
 		}
