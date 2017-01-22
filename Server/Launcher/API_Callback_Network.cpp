436e3c000f1226755a47b31b3f62d0aa6b69146e
Andy <nhm.nohalfmeasures@gmail.com>
2017-01-22 16:22:38 +0000

    fixed derp in api_onplayerconnecting/ed that caused linux crash

--- Server/Launcher/API_Callback_Network.cpp
+++ Server/Launcher/API_Callback_Network.cpp
@@ -14,7 +14,7 @@ namespace API
 #ifdef WIN32
 				API_OnPlayerConnecting_t API_OnPlayerConnecting = (API_OnPlayerConnecting_t)::GetProcAddress((HMODULE)Instance, "API_OnPlayerConnecting");
 #else
-				API_OnPlayerConnecting_t API_OnPlayerConnecting = (API_OnPlayerConnecting_t)dlsym(&Instance, "API_OnPlayerConnecting");
+				API_OnPlayerConnecting_t API_OnPlayerConnecting = (API_OnPlayerConnecting_t)dlsym(Instance, "API_OnPlayerConnecting");
 #endif
 				return API_OnPlayerConnecting(guid);
 			}
@@ -29,7 +29,7 @@ namespace API
 #ifdef WIN32
 				API_OnPlayerConnected_t API_OnPlayerConnected = (API_OnPlayerConnected_t)::GetProcAddress((HMODULE)Instance, "API_OnPlayerConnected");
 #else
-				API_OnPlayerConnected_t API_OnPlayerConnected = (API_OnPlayerConnected_t)dlsym(&Instance, "API_OnPlayerConnected");
+				API_OnPlayerConnected_t API_OnPlayerConnected = (API_OnPlayerConnected_t)dlsym(Instance, "API_OnPlayerConnected");
 #endif
 				return API_OnPlayerConnected(entity, playerid);
 			}
