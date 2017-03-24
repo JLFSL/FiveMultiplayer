ff0c05a13c40779484ab47fe32ba17407392b3f9
Andy <nhm.nohalfmeasures@gmail.com>
2017-03-24 13:56:38 +0000

    Fixed local clients model not being set/updated

--- Client/Core/CRPCPlayer.cpp
+++ Client/Core/CRPCPlayer.cpp
@@ -2,10 +2,8 @@
 
 void CRPCPlayer::PlayerModel(RakNet::BitStream *bitStream, RakNet::Packet *packet)
 {
-	int entity;
 	RakString model;
 
-	bitStream->Read(entity);
 	bitStream->Read(model);
 
 	GamePed::SetPedModel(g_Core->GetLocalPlayer()->GetPed(), model.C_String());
