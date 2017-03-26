6e98a052663565f2cba494318c7db60ae9572e56
FiveMP <developer@five-multiplayer.net>
2017-03-26 22:18:52 +0200

    - Mousehook updated, should work for everything apart from RMB - Fixed API.Base's errors

--- Client/Core/CefMouseHook.cpp
+++ Client/Core/CefMouseHook.cpp
@@ -53,95 +53,130 @@ void CefMouseHook::ForegroundCheck() {
 }
 
 LRESULT WINAPI MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam) {
-	if (nCode == 0 && CefMouseHook::IsVisible()) {
-		//ImGuiIO& io = ImGui::GetIO();
-
+	if (nCode == 0 && CefMouseHook::IsVisible()) 
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		
 		CefMouseEvent mouseEvent;
 		POINT pos;
 		RECT rect;
+		short delta;
+		MSLLHOOKSTRUCT *pMhs;
 
-		switch (wParam) {
-		case WM_LBUTTONDOWN:
-			//io.MouseDown[0] = true;
+		switch (wParam) 
+		{
+			case WM_LBUTTONDOWN:
+				io.MouseDown[0] = true;
 
-			GetCursorPos(&pos);
-			mouseEvent.x = pos.x;
-			mouseEvent.y = pos.y;
-			mouseEvent.modifiers = EVENTFLAG_LEFT_MOUSE_BUTTON;
-			CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_LEFT, false, 1);
-			break;
-		case WM_LBUTTONUP:
-			//io.MouseDown[0] = false;
+				GetWindowRect(CefRenderer::getProcess(), &rect);
+				GetCursorPos(&pos);
 
-			GetCursorPos(&pos);
-			mouseEvent.x = pos.x;
-			mouseEvent.y = pos.y;
-			mouseEvent.modifiers = EVENTFLAG_LEFT_MOUSE_BUTTON;
-			CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_LEFT, true, 1);
-			break;
-		case WM_RBUTTONDOWN:
-			//io.MouseDown[1] = true;
-			break;
-		case WM_RBUTTONUP:
-			//io.MouseDown[1] = false;
+				mouseEvent.x = (pos.x - rect.left);
+				mouseEvent.y = (pos.y - rect.top);
+				mouseEvent.modifiers = EVENTFLAG_LEFT_MOUSE_BUTTON;
 
-			GetCursorPos(&pos);
-			mouseEvent.x = pos.x;
-			mouseEvent.y = pos.y;
-			mouseEvent.modifiers = EVENTFLAG_RIGHT_MOUSE_BUTTON;
-			CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_RIGHT, true, 1);
-			break;
-		case WM_MBUTTONDOWN:
-			//io.MouseDown[2] = true;
-			break;
-		case WM_MBUTTONUP:
-			//io.MouseDown[2] = false;
+				CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_LEFT, false, 1);
+				break;
+			case WM_LBUTTONUP:
+				io.MouseDown[0] = false;
 
-			GetCursorPos(&pos);
-			mouseEvent.x = pos.x;
-			mouseEvent.y = pos.y;
-			mouseEvent.modifiers = EVENTFLAG_MIDDLE_MOUSE_BUTTON;
-			CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_MIDDLE, true, 1);
-			break;
-		case WM_MOUSEWHEEL: {
-			MSLLHOOKSTRUCT *pMhs = (MSLLHOOKSTRUCT *)lParam;
-			short zDelta = HIWORD(pMhs->mouseData);
+				GetWindowRect(CefRenderer::getProcess(), &rect);
+				GetCursorPos(&pos);
 
-			if (zDelta > 0) {
-				//io.MouseWheel += 0.2f;
-			}
-			else {
-				//io.MouseWheel -= 0.2f;
-			}
-			break;
-		}
-		case WM_MOUSEMOVE:
-			//io.MousePos.x = (signed short)(lParam);
-			//io.MousePos.y = (signed short)(lParam >> 16);
+				mouseEvent.x = (pos.x - rect.left);
+				mouseEvent.y = (pos.y - rect.top);
+				mouseEvent.modifiers = EVENTFLAG_LEFT_MOUSE_BUTTON;
 
-			
-			GetWindowRect(CefRenderer::getProcess(), &rect);
+				CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_LEFT, true, 1);
+				break;
+			case WM_RBUTTONDOWN:
+				io.MouseDown[1] = true;
 
-			GetCursorPos(&pos);
-			mouseEvent.x = ( pos.x - rect.left);
-			mouseEvent.y = ( pos.y - rect.top);
-			mouseEvent.modifiers = EVENTFLAG_NONE;
-			
-			CefRenderer::getBrowser()->GetHost()->SendMouseMoveEvent(mouseEvent, false);
-			break;
-		case WM_KEYDOWN:
-			if (wParam < 256)
-				//io.KeysDown[wParam] = 1;
-			break;
-		case WM_KEYUP:
-			if (wParam < 256)
-				//io.KeysDown[wParam] = 0;
-			break;
-		case WM_CHAR:
-			// You can also use ToAscii()+GetKeyboardState() to retrieve characters.
-			if (wParam > 0 && wParam < 0x10000)
-				//io.AddInputCharacter((unsigned short)wParam);
-			break;
+				/*
+					GetWindowRect(CefRenderer::getProcess(), &rect);
+
+					GetCursorPos(&pos);
+					mouseEvent.x = (pos.x - rect.left);
+					mouseEvent.y = (pos.y - rect.top);
+					mouseEvent.modifiers = EVENTFLAG_RIGHT_MOUSE_BUTTON;
+
+					CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_RIGHT, false, 1);
+				*/
+				break;
+			case WM_RBUTTONUP:
+				io.MouseDown[1] = false;
+
+				/*
+					GetWindowRect(CefRenderer::getProcess(), &rect);
+					GetCursorPos(&pos);
+
+					mouseEvent.x = (pos.x - rect.left);
+					mouseEvent.y = (pos.y - rect.top);
+					mouseEvent.modifiers = EVENTFLAG_RIGHT_MOUSE_BUTTON;
+
+					CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_RIGHT, true, 1);
+				*/
+				break;
+			case WM_MBUTTONDOWN:
+				io.MouseDown[2] = true;
+
+				GetWindowRect(CefRenderer::getProcess(), &rect);
+				GetCursorPos(&pos);
+
+				mouseEvent.x = (pos.x - rect.left);
+				mouseEvent.y = (pos.y - rect.top);
+				mouseEvent.modifiers = EVENTFLAG_MIDDLE_MOUSE_BUTTON;
+
+				CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_MIDDLE, false, 1);
+				break;
+			case WM_MBUTTONUP:
+				io.MouseDown[2] = false;
+
+				GetWindowRect(CefRenderer::getProcess(), &rect);
+				GetCursorPos(&pos);
+
+				mouseEvent.x = (pos.x - rect.left);
+				mouseEvent.y = (pos.y - rect.top);
+				mouseEvent.modifiers = EVENTFLAG_MIDDLE_MOUSE_BUTTON;
+
+				CefRenderer::getBrowser()->GetHost()->SendMouseClickEvent(mouseEvent, MBT_MIDDLE, true, 1);
+				break;
+			case WM_MOUSEWHEEL:
+				pMhs = (MSLLHOOKSTRUCT *)lParam;
+				delta = HIWORD(pMhs->mouseData);
+				
+				GetWindowRect(CefRenderer::getProcess(), &rect);
+				GetCursorPos(&pos);
+
+				mouseEvent.x = (pos.x - rect.left);
+				mouseEvent.y = (pos.y - rect.top);
+
+				CefRenderer::getBrowser()->GetHost()->SendMouseWheelEvent(mouseEvent, 0, delta);
+				break;
+			case WM_MOUSEMOVE:
+				io.MousePos.x = (signed short)(lParam);
+				io.MousePos.y = (signed short)(lParam >> 16);
+				GetWindowRect(CefRenderer::getProcess(), &rect);
+
+				GetCursorPos(&pos);
+				mouseEvent.x = (pos.x - rect.left);
+				mouseEvent.y = (pos.y - rect.top);
+				mouseEvent.modifiers = EVENTFLAG_NONE;
+
+				CefRenderer::getBrowser()->GetHost()->SendMouseMoveEvent(mouseEvent, false);
+				break;
+			case WM_KEYDOWN:
+				if (wParam < 256)
+					io.KeysDown[wParam] = 1;
+				break;
+			case WM_KEYUP:
+				if (wParam < 256)
+					io.KeysDown[wParam] = 0;
+				break;
+			case WM_CHAR:
+				if (wParam > 0 && wParam < 0x10000)
+					io.AddInputCharacter((unsigned short)wParam);
+				break;
 		}
 	}
 	return CallNextHookEx(CefMouseHook::GetHook(), nCode, wParam, lParam);
