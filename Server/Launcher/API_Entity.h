cbae447e7afb5fa86a7646b47b2fc6b3a5d415e9
Andy <nhm.nohalfmeasures@gmail.com>
2017-04-02 19:28:39 +0100

    Removed Struct that ar eno longer used

--- Server/Launcher/API_Entity.h
+++ Server/Launcher/API_Entity.h
@@ -7,46 +7,6 @@ extern "C" {
 			static const char *ThisNamespace;
 
 		public:
-			/*DLL_PUBLIC struct PedComponent
-			{
-				int drawableid = -1;
-				int textureid = -1;
-				int paletteid = -1;
-			};
-
-			DLL_PUBLIC struct PedHeadBlend
-			{
-				int shapeFirst = 0;
-				int shapeSecond = 0;
-				int shapeThird = 0;
-				int skinFirst = 0;
-				int skinSecond = 0;
-				int skinThird = 0;
-				float shapeMix = 0;
-				float skinMix = 0;
-				float thirdMix = 0;
-			};
-
-			DLL_PUBLIC struct PedHeadOverlay
-			{
-				int index = 0;
-				float opacity = 0.0f;
-				int colorType = 0;
-				int colorID = 0;
-				int secondColorID = 0;
-			};
-
-			DLL_PUBLIC struct PedProp
-			{
-				int drawableid = 0;
-				int textureid = 0;
-			};
-
-			DLL_PUBLIC struct PedFeature
-			{
-				float scale = 0.0f;
-			};*/
-
 			DLL_PUBLIC static void Destroy(const int entity);
 			DLL_PUBLIC static const CVector3 GetPosition(const int entity);
 			DLL_PUBLIC static void SetPosition(const int entity, const CVector3 position);
