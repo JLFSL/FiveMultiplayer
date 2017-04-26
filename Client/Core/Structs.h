3917acdd88c935a20620d92a7b45773e079e962a
Andy <nhm.nohalfmeasures@gmail.com>
2017-04-26 12:21:12 +0100

    small update to the structs of sudo

--- Client/Core/Structs.h
+++ Client/Core/Structs.h
@@ -1,58 +1,59 @@
-#pragma once
+#ifndef __SUDOSTRUCTS_H__
+#define __SUDOSTRUCTS_H__
 
 #ifdef __SC_VERSION__
-#define ADDRESS_WORLD					0x2328EC8		//42
-#define ADDRESS_WAYPOINT				0x1F7F400		//wtb a (mini)map struct
-#define ADDRESS_OBJECTIVE				0x1F7F4B0
-#define ADDRESS_AMMO					0x0F706C5		//Ammo dec code; 41 2B D1 E8; 90 90 90 E8
-#define ADDRESS_MAGAZINE				0x0F70680		//Magazine dec code; 41 2B C9 3B C8 0F; 90 90 90 3B C8 0F
+const int ADDRESS_WORLD			= 0x2328EC8;		//42
+const int ADDRESS_WAYPOINT		= 0x1F7F400;		//wtb a (mini)map struct
+const int ADDRESS_OBJECTIVE		= 0x1F7F4B0;
+const int ADDRESS_AMMO			= 0x0F706C5;		//Ammo dec code; 41 2B D1 E8; 90 90 90 E8
+const int ADDRESS_MAGAZINE		= 0x0F70680;		//Magazine dec code; 41 2B C9 3B C8 0F; 90 90 90 3B C8 0F
 #endif
 #ifdef __STEAM_VERSION__
-#define ADDRESS_WORLD					0x232BC00
-#define ADDRESS_WAYPOINT				0x1F82460
-#define ADDRESS_OBJECTIVE				0x1F82510
-#define ADDRESS_AMMO					0x0F71C38
-#define ADDRESS_MAGAZINE				0x0F71C7D
+const int ADDRESS_WORLD			= 0x232BC00;
+const int ADDRESS_WAYPOINT		= 0x1F82460;
+const int ADDRESS_OBJECTIVE		= 0x1F82510;
+const int ADDRESS_AMMO			= 0x0F71C38;
+const int ADDRESS_MAGAZINE		= 0x0F71C7D;
 #endif
 
 //offset from WORLD
-#define OFFSET_PLAYER					0x08			//playerbase
+const int OFFSET_PLAYER					= 0x08;			//playerbase
 
 //entity offsets
-#define OFFSET_ENTITY_POSBASE			0x30			//base of a structure that contains entity coords
-#define OFFSET_ENTITY_POSBASE_POS		0x50			//vector3
-#define OFFSET_ENTITY_POS				0x90			//vector3
-#define OFFSET_ENTITY_HEALTH			0x280			//entity health (except for vehicles); float cur, float max
-#define OFFSET_ENTITY_HEALTH_MAX		0x2A0			//they moved this away from curHealth in 1.36 :(
-#define OFFSET_ENTITY_ATTACKER			0x2A8			//base to a list of the last 3 entities that attacked the current entity
-#define OFFSET_ENTITY_GOD				0x189			//godmode; on = 1, off = 0; byte
+const int OFFSET_ENTITY_POSBASE			= 0x30;			//base of a structure that contains entity coords
+const int OFFSET_ENTITY_POSBASE_POS		= 0x50;			//vector3
+const int OFFSET_ENTITY_POS				= 0x90;			//vector3
+const int OFFSET_ENTITY_HEALTH			= 0x280;		//entity health (except for vehicles); float cur, float max
+const int OFFSET_ENTITY_HEALTH_MAX		= 0x2A0;		//they moved this away from curHealth in 1.36 :(
+const int OFFSET_ENTITY_ATTACKER		= 0x2A8;		//base to a list of the last 3 entities that attacked the current entity
+const int OFFSET_ENTITY_GOD				= 0x189;		//godmode; on = 1, off = 0; byte
 
 //player (entity) offsets
-#define OFFSET_PLAYER_ARMOR						0x14B0			//armour
-#define OFFSET_PLAYER_INFO						0x10B8			//playerInfo struct
-#define OFFSET_PLAYER_INFO_WANTED_CAN_CHANGE	0x71C			//fWantedCanChange
-#define OFFSET_PLAYER_INFO_WANTED				0x798			//wanted level; DWORD
-#define	OFFSET_PLAYER_INFO_RUN_SPD				0xE8			//run speed; def 1; float
-#define OFFSET_PLAYER_INFO_SWIM_SPD				0xE4			//swim speed; def 1; float
-#define OFFSET_PLAYER_INFO_FRAMEFLAGS			0x190			//frame flags; DWORD
-#define OFFSET_PLAYER_INFO_STAMINA				0xC00			//fStamina, fStaminaMax
-#define OFFSET_PLAYER_VEHICLE					0xD28			//ptr to last used vehicle
-#define OFFSET_PLAYER_NAME						0x7C
-#define OFFSET_PLAYER_RAGDOLL					0x10A8			//byte; CPed.noRagdoll: 0x20 = off; 0x00/0x01 = on
-#define OFFSET_PLAYER_SEATBELT					0x13EC			//byte; CPed.seatBelt: 0xC8 = off; 0xC9 = on
-#define OFFSET_PLAYER_INVEHICLE					0x146B
+const int OFFSET_PLAYER_ARMOR					= 0x14B0;		//armour
+const int OFFSET_PLAYER_INFO					= 0x10B8;		//playerInfo struct
+const int OFFSET_PLAYER_INFO_WANTED_CAN_CHANGE	= 0x71C;		//fWantedCanChange
+const int OFFSET_PLAYER_INFO_WANTED				= 0x798;		//wanted level; DWORD
+const int OFFSET_PLAYER_INFO_RUN_SPD			= 0xE8;			//run speed; def 1; float
+const int OFFSET_PLAYER_INFO_SWIM_SPD			= 0xE4;			//swim speed; def 1; float
+const int OFFSET_PLAYER_INFO_FRAMEFLAGS			= 0x190;		//frame flags; DWORD
+const int OFFSET_PLAYER_INFO_STAMINA			= 0xC00;		//fStamina, fStaminaMax
+const int OFFSET_PLAYER_VEHICLE					= 0xD28;		//ptr to last used vehicle
+const int OFFSET_PLAYER_NAME					= 0x7C;
+const int OFFSET_PLAYER_RAGDOLL					= 0x10A8;		//byte; CPed.noRagdoll: 0x20 = off; 0x00/0x01 = on
+const int OFFSET_PLAYER_SEATBELT				= 0x13EC;		//byte; CPed.seatBelt: 0xC8 = off; 0xC9 = on
+const int OFFSET_PLAYER_INVEHICLE				= 0x146B;
 
 //vehicle offsets
-#define OFFSET_VEHICLE_HEALTH						0x84C			//vehicle health; 0.f-1000.f
-#define OFFSET_VEHICLE_GRAVITY						0xB7C			//fGravity
-#define OFFSET_VEHICLE_BULLETPROOF_TIRES			0x883			//btBulletproofTires;  (btBulletproofTires & 0x20) ? true : false
-#define OFFSET_VEHICLE_HANDLING						0x878
-#define OFFSET_VEHICLE_HANDLING_ACCELERATION		0x4C
-#define OFFSET_VEHICLE_HANDLING_BRAKEFORCE			0x6C
-#define OFFSET_VEHICLE_HANDLING_TRACTION_CURVE_MIN	0x90			//fTractionCurveMin
-#define OFFSET_VEHICLE_HANDLING_DEFORM_MULTIPLIER	0xF8			//fDeformationDamageMult
-#define OFFSET_VEHICLE_HANDLING_UPSHIFT				0x58
-#define OFFSET_VEHICLE_HANDLING_SUSPENSION_FORCE	0xBC			//fSuspensionForce 
+const int OFFSET_VEHICLE_HEALTH							= 0x84C;	//vehicle health; 0.f-1000.f
+const int OFFSET_VEHICLE_GRAVITY						= 0xB7C;	//fGravity
+const int OFFSET_VEHICLE_BULLETPROOF_TIRES				= 0x883;	//btBulletproofTires;  (btBulletproofTires & 0x20) ? true : false
+const int OFFSET_VEHICLE_HANDLING						= 0x878;
+const int OFFSET_VEHICLE_HANDLING_ACCELERATION			= 0x4C;
+const int OFFSET_VEHICLE_HANDLING_BRAKEFORCE			= 0x6C;
+const int OFFSET_VEHICLE_HANDLING_TRACTION_CURVE_MIN	= 0x90;		//fTractionCurveMin
+const int OFFSET_VEHICLE_HANDLING_DEFORM_MULTIPLIER		= 0xF8;		//fDeformationDamageMult
+const int OFFSET_VEHICLE_HANDLING_UPSHIFT				= 0x58;
+const int OFFSET_VEHICLE_HANDLING_SUSPENSION_FORCE		= 0xBC;		//fSuspensionForce 
 
 //weapon offsets
 const int OFFSET_WEAPON_MANAGER				= 0x10C8;		//from playerbase
@@ -120,3 +121,5 @@ public:
 		return *(CReplayInterface**)(Hooking::getReplayPtr);
 	}
 };
+
+#endif
