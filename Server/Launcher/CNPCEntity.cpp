7838235655100ffbe2ecf2d0b5d24c0c5a55e5c6
Andy <nhm.nohalfmeasures@gmail.com>
2017-04-27 20:33:00 +0100

    NPC Model output fix

--- Server/Launcher/CNPCEntity.cpp
+++ Server/Launcher/CNPCEntity.cpp
@@ -17,7 +17,7 @@ void CNPCEntity::Create(const std::wstring model, const CVector3 position, const
 
 	Amount++;
 
-	std::cout << "[CNPCEntity] Created NPC [" << Data.Id << "] with model " << Data.Model.Model.C_String() << " at " << Data.Position.x << ", " << Data.Position.y << ", " << Data.Position.z << std::endl;
+	std::wcout << L"[CNPCEntity] Created NPC [" << Data.Id << L"] with model " << Data.Model.Model.C_String() << L" at " << Data.Position.x << L", " << Data.Position.y << L", " << Data.Position.z << std::endl;
 	std::cout << "[CNPCEntity] " << Amount << " npcs in the world." << std::endl;
 
 	Network.LastSyncSent = std::chrono::system_clock::now();
