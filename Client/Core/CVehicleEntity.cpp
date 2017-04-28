87b89bd3c50c95ffbcadb99528a180784b8af214
FiveMP <developer@five-multiplayer.net>
2017-04-28 13:49:04 +0200

    derp

--- Client/Core/CVehicleEntity.cpp
+++ Client/Core/CVehicleEntity.cpp
@@ -335,6 +335,7 @@ void CVehicleEntity::Update(Packet * packet)
 			UpdateTargetPosition();
 			SetTargetData();
 			UpdateTargetRotation();
+			Network.LastSyncReceived = timeGetTime();
 		}
 	}
 	else
@@ -342,6 +343,7 @@ void CVehicleEntity::Update(Packet * packet)
 		UpdateTargetPosition();
 		SetTargetData();
 		UpdateTargetRotation();
+		Network.LastSyncReceived = timeGetTime();
 	}
 }
 
